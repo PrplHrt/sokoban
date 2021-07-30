cd60b081d63deb8b3c7c80c37540cf8110596212
Eyad Mohamed Ali <eyad.azim@gmail.com>
2021-07-30 15:54:27 +0400

    Put the parser into a class

--- level_parser.py
+++ level_parser.py
@@ -1,6 +1,22 @@
 f = open("levels/lvl_1.txt")
 
+level = []
+
 for line in f:
-    print(line, end="")
+    # Creating list of character lists, excluding the newline character
+    level.append(list(line[:-1]))
+
+print(level)
 
 f.close()
+
+class game():
+    def __init__(self):
+        f = open("levels/lvl_1.txt")
+        self.level = []
+        for line in f:
+            # Creating list of character lists, excluding the newline character
+            self.level.append(list(line[:-1]))
+        f.close()
+
+    
