196b7edf5a636783e812942eacfc3f8bd95538f8
Eyad Mohamed Ali <eyad.azim@gmail.com>
2021-08-02 18:35:50 +0400

    Added more levels and made the level parser work

--- level_parser.py
+++ level_parser.py
@@ -2,7 +2,7 @@ import arcade as arc
 
 # Sizes of elements on the screen
 SPRITE_NATIVE_SIZE = 128
-SPRITE_SCALAR = 0.25
+SPRITE_SCALAR = 0.3
 SPRITE_SIZE = int(SPRITE_NATIVE_SIZE * SPRITE_SCALAR)
 
 # Number of cells for each level
@@ -15,7 +15,7 @@ SCREEN_HEIGHT = SPRITE_SIZE * HEIGHT
 SCREEN_TITLE = "Sokoban"
 
 # Symbol parsing
-FLOOR = " "
+FLOOR = "-"
 WALL = "#"
 PLAYER = "@"
 TARGET  = "."
@@ -23,13 +23,14 @@ BOX = "$"
 BOX_ON_TARGET = "*"
 PLAYER_ON_TARGET = "+"
 
-SPRITES1 = {
+SPRITES = {
     FLOOR : "assets/meriam_sokoban_sprites/floor.jpg",
     WALL: "assets/meriam_sokoban_sprites/wall.png",
     PLAYER: "assets/meriam_sokoban_sprites/player.png",
     TARGET: "assets/meriam_sokoban_sprites/target.png",
     BOX: "assets/meriam_sokoban_sprites/box.png",
     BOX_ON_TARGET:"assets/meriam_sokoban_sprites/box_on_target.png",
+    PLAYER_ON_TARGET:"assets/meriam_sokoban_sprites/floor.jpg"
 }
 
 # Created child class of arcade.Window
@@ -41,48 +42,48 @@ class MyWindow(arc.Window):
 
         self.get_level()
 
-    def get_level(self, file_name = "levels/lvl_1.txt"):
-        f = open("levels/lvl_1.txt")
+    def get_level(self, file_name = "assets/levels/lvl_1.txt"):
+        f = open(file_name)
         self.level = []
         self.num_boxes = 0
         for line in f:
-            # Getting numbe of boxes in level
-            for char in line:
-                if char == BOX or char == BOX_ON_TARGET:
-                    self.num_boxes += 1
-
             # Creating list of character lists, excluding the newline character
-            self.level.append(list(line[:-1]))
+            self.level.append(list(line[:-1].replace("\t", "")))
         f.close()
 
     # Refreshes the screen
     def on_draw(self):
         arc.start_render()
-        # Create sprite by getting the sprite image
-        self.player = arc.Sprite("player.png", SPRITE_SCALAR)
-        # Place the sprite at (100,100)
-        self.player.center_x = 700
-        self.player.center_y = 100
-        # Draw the sprite
-        self.player.draw()
+        # Create all the sprites and render the level based on the level matrix
+        sprites = arc.SpriteList()
+        for i in range(HEIGHT):
+            for j in range(WIDTH):
+                floor = self.make_sprite(FLOOR, i, j)
+                sprites.append(floor)
 
-        # Can create many sprites at the same time using a sprite list
-        self.boxes = arc.SpriteList()
-
-        for i in range(self.num_boxes):
-            box = arc.Sprite("assets/meriam_sokoban_sprites/box.png", SPRITE_SCALAR)
-            box.center_x = 70 * (i+1)
-            box.center_y = 50 * (i+1)
-            self.boxes.append(box)
-        
-        self.boxes.draw()
+                if self.level[i][j] == FLOOR:
+                    continue
 
 
-        
+                sprite = self.make_sprite(self.level[i][j], i, j)
+                sprites.append(sprite)
+
+                if self.level[i][j] == PLAYER_ON_TARGET:
+                    sprite = self.make_sprite(PLAYER, i, j)
+                    sprites.append(sprite)
+
+        sprites.draw()
+
+    # Helper function to create a sprite on specific coordinates
+    def make_sprite(self, cell, i, j):
+        sprite = arc.Sprite(SPRITES[cell], SPRITE_SCALAR)
+        sprite.center_x = j * SPRITE_SIZE + (SPRITE_SIZE // 2)
+        sprite.center_y = (HEIGHT - 1 - i) * SPRITE_SIZE + (SPRITE_SIZE // 2)
+        return sprite
 
 def main():
     # Creating game window with width, height, and title
-    window = MyWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_HEIGHT)
+    window = MyWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arc.run()
 
 
