b510cb4979d39535eaf81a91ac845687e89b5444
Eyad Mohamed Ali <eyad.azim@gmail.com>
2021-08-03 15:46:27 +0400

    Added movement and more assets

--- level_parser.py
+++ level_parser.py
@@ -15,7 +15,7 @@ SCREEN_HEIGHT = SPRITE_SIZE * HEIGHT
 SCREEN_TITLE = "Sokoban"
 
 # Symbol parsing
-FLOOR = "-"
+FLOOR = " "
 WALL = "#"
 PLAYER = "@"
 TARGET  = "."
@@ -29,23 +29,20 @@ SPRITES = {
     PLAYER: "assets/meriam_sokoban_sprites/player.png",
     TARGET: "assets/meriam_sokoban_sprites/target.png",
     BOX: "assets/meriam_sokoban_sprites/box.png",
-    BOX_ON_TARGET:"assets/meriam_sokoban_sprites/box_on_target.png",
+    BOX_ON_TARGET:"assets/meriam_sokoban_sprites/box_on_target.jpg",
     PLAYER_ON_TARGET:"assets/meriam_sokoban_sprites/floor.jpg"
 }
 
 # Created child class of arcade.Window
-class MyWindow(arc.Window):
-    def __init__(self, w: int, h: int, t: str):
+class LevelWindow(arc.Window):
+    def __init__(self, w: int, h: int, t: str, f = "lvl_3.txt"):
         # Prepare the Window
         super().__init__(w, h, t)
         arc.set_background_color(arc.color.AMARANTH_PURPLE)
 
-        self.get_level()
-
-    def get_level(self, file_name = "assets/levels/lvl_1.txt"):
-        f = open(file_name)
+        
+        f = open("assets/levels/" + f)
         self.level = []
-        self.num_boxes = 0
         for line in f:
             # Creating list of character lists, excluding the newline character
             self.level.append(list(line[:-1].replace("\t", "")))
@@ -64,7 +61,6 @@ class MyWindow(arc.Window):
                 if self.level[i][j] == FLOOR:
                     continue
 
-
                 sprite = self.make_sprite(self.level[i][j], i, j)
                 sprites.append(sprite)
 
@@ -81,9 +77,81 @@ class MyWindow(arc.Window):
         sprite.center_y = (HEIGHT - 1 - i) * SPRITE_SIZE + (SPRITE_SIZE // 2)
         return sprite
 
+    def inside(self, i, j):
+        return 0 <= i < HEIGHT and 0 <= j < WIDTH
+
+    def move_player(self, di, dj):
+        # Find player first
+        pi, pj = -1, -1
+        for i in range(len(self.level)):
+            for j in range(len(self.level[i])):
+                if self.level[i][j] in [PLAYER , PLAYER_ON_TARGET]:
+                    pi, pj = i, j
+                    break
+        
+        under_player = FLOOR 
+        if self.level[pi][pj] != PLAYER:
+            under_player = TARGET
+
+        cell1_i = pi + di
+        cell1_j = pj + dj
+
+        if not self.inside(cell1_i, cell1_j):
+            return
+        
+        cell1 = self.level[cell1_i][cell1_j]
+
+        if cell1 == WALL:
+            return
+        
+        if cell1 == FLOOR:
+            self.level[pi][pj] = under_player
+            self.level[cell1_i][cell1_j] = PLAYER
+            return
+        
+        if cell1 == TARGET:
+            self.level[pi][pj] = under_player
+            self.level[cell1_i][cell1_j] = PLAYER_ON_TARGET
+            return
+        
+        # if BOX or BOX_ON_TARGET is in cell1
+        cell2_i, cell2_j = cell1_i + di, cell1_j + dj
+        
+        if not self.inside(cell2_i, cell2_j):
+            return
+        
+        cell2 = self.level[cell2_i][cell2_j]
+
+        if cell2 in [BOX, BOX_ON_TARGET, WALL]:
+            return
+
+        self.level[pi][pj] = under_player
+
+        if cell1 == BOX_ON_TARGET:
+            self.level[cell1_i][cell1_j] = PLAYER_ON_TARGET
+        elif cell1 == BOX:
+            self.level[cell1_i][cell1_j] = PLAYER
+
+        if cell2 == FLOOR:
+            self.level[cell2_i][cell2_j] = BOX
+        else:
+            self.level[cell2_i][cell2_j] = BOX_ON_TARGET
+
+
+   
+    def on_key_press(self, key: int, modifiers: int):
+        if key == arc.key.UP or key == arc.key.W:
+            self.move_player(-1, 0)
+        if key == arc.key.DOWN or key == arc.key.S:
+            self.move_player(1, 0)
+        if key == arc.key.RIGHT or key == arc.key.D:
+            self.move_player(0, 1)
+        if key == arc.key.LEFT or key == arc.key.A:
+            self.move_player(0, -1)
+
 def main():
     # Creating game window with width, height, and title
-    window = MyWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = LevelWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arc.run()
 
 
